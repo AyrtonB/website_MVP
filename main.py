8aef8337ba0e55f90856bcbf6e6d75f0fc031ac5
AyrtonB <ayrtonbourn@outlook.com>
2020-02-01 13:47:26 +0000

    colored_map

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
@@ -9,6 +10,15 @@ def home():
 @app.route("/map")
 def map():
     return render_template("map.html", img_url='../static/img/boiling_bubbles.jpg')
+
+@app.route("/data")
+def data():
+    with open('static/data/test.json', 'r') as f:
+        data = json.load(f)
+    
+    return data
+
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
